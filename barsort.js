ac44d2eb8ca3e3dcee3d811ae1d05d181286ae85
andrews <andrewstrain@pericosm.com>
2017-05-10 17:06:01 +0100

    a typo broke fullsort

--- barsort.js
+++ barsort.js
@@ -7,22 +7,22 @@
 var Barsortfactory = function(){ return (function(){ 
   'use strict'
 
-  var cntofsub=[],destroom=[],destofsub=[]
+  var _cntofsub=[],_destrema=[],_destosub=[]
   var wkcnt=0,wkcyc=100 //these count use of arrays
 
-  function version(){ return "0.5.0" }
+  function version(){ return "0.6.0" }
 
   function ranges(pr){ //barnum:,scores:,st:,ov:,keysbar:,barfreq:,burnscore:
     
     var barnm  = pr.barnum
        ,scores = pr.scores
        ,barndx = pr.keysbar||[]
-       ,sectppl= pr.barpopl||[]
+       ,sectppl= pr.barpopl
        ,st=pr.st||0 
        ,ov=pr.ov||scores.length
        ,resol=pr.resolution||5  //over sample x5
        
-    if(!pr.burnscore){
+    if(!pr.burnscore){  //clone score
       scores=[]
       for(var i=st; i<ov; i++){
         scores[i]=pr.scores[i]
@@ -32,6 +32,7 @@ var Barsortfactory = function(){ return (function(){
     var avg=-0 , val=-0 ,nb=ov-st
     
     var minv=scores[st],maxv=scores[st] 
+    var lowex=-281474976710656
     
     for(var i=st; i<ov; i++) //need to max,min 
     { 
@@ -40,8 +41,8 @@ var Barsortfactory = function(){ return (function(){
       //~ val=Math.sqrt(
         //~ jote.x[j]*jote.x[j]+jote.y[j]*jote.y[j]+jote.z[j]*jote.z[j]
       //~ )
-      if(isNaN(scores[i])) scores[i]=0
-      val=scores[i]||0
+      if(!(scores[i]>lowex)){ scores[i]=lowex }  //but score should not be NaN!
+      val=scores[i]||0 
       avg+=val
       if (val>maxv){ maxv=val }
       if (val<minv){ minv=val }
@@ -49,7 +50,7 @@ var Barsortfactory = function(){ return (function(){
      
     avg/=nb, avg-=minv, maxv-=minv
 
-    var hiv=0,havg=-0
+    var hiv=0,havg=-0 // highvals, highavg
     
     for(var i=st; i<ov; i++) 
     { val=scores[i]-=minv 
@@ -63,34 +64,30 @@ var Barsortfactory = function(){ return (function(){
     
     //~ console.log(subdvn)
     //recycle these temp arrays
-    //var cntofsub=new Array(subdvn), destofsub=new Array(subdvn)
+    //var _cntofsub=new Array(subdvn), _destosub=new Array(subdvn)
     
-    if (cntofsub.length<subdvn 
-     ||(wkcnt++>wkcyc&&cntofsub.length>subdvn)){
-      cntofsub   = new Array(subdvn) 
-      destofsub = new Array(subdvn)
+    if( _cntofsub.length<subdvn 
+     ||( wkcnt++>wkcyc && _cntofsub.length>subdvn) ){
+      _cntofsub = new Array(subdvn) 
+      _destosub = new Array(subdvn)
       wkcnt=0
-    }
-    
-    for(var ch=0; ch<subdvn; ch++){ cntofsub[ch]=0 }
-    
-    if (destroom.length<barnm){ destroom=new Array(barnm) }
-    
-    for(var ch=0; ch<barnm; ch++){ destroom[ch]=0 }
+    } //just maintaining workspace arrays here ...
+    for(var ch=0; ch<subdvn; ch++){ _cntofsub[ch]=0 }
+    if (_destrema.length<barnm){ _destrema=new Array(barnm) }
+    for(var ch=0; ch<barnm; ch++){ _destrema[ch]=0 }
     
     for(var i=st; i<ov; i++){
       
       val=Math.floor(subdvn*scores[i]/maxv)
       val=(val<subdvn)?val:subdvn-1    //to opt this out increase maxv ?
                                      //no, maxv is set deliberately low
-      cntofsub[scores[i]=val]++    //! scores is written here
+      _cntofsub[scores[i]=val]++    //! scores is written here
       
     }
     
     var rlbar=(nb/barnm)
        ,fllbar=0,fldbar=0,spills=[]
        ,nxtcap=rlbar+0.5, fcap=Math.floor(nxtcap)
-    
     //largest subdv is sometimes getting a key - fixit....
     
     //determining sub anchors to bar anchors  (anchor is first address)
@@ -99,12 +96,12 @@ var Barsortfactory = function(){ return (function(){
     
     for(var sub=0; sub<subdvn; sub++){
       
-      destofsub[sub]=fllbar            //cntofsub bar sub goes to dest[fllbar]
-      destroom[fllbar]+=cntofsub[sub]  //destroom[fllbar] gets population of bar sub
+      _destosub[sub]=fllbar            //_cntofsub bar sub goes to dest[fllbar]
+      _destrema[fllbar]+=_cntofsub[sub]  //_destrema[fllbar] gets population of bar sub
 
-      while(destroom[fllbar]>=fcap){
-        destroom[fllbar+1]+=destroom[fllbar]-fcap
-        destroom[fllbar]=fcap
+      while(_destrema[fllbar]>=fcap){
+        _destrema[fllbar+1]+=_destrema[fllbar]-fcap
+        _destrema[fllbar]=fcap
         fllbar++
         nxtcap+=rlbar-fcap
         fcap=Math.floor(nxtcap)
@@ -113,20 +110,21 @@ var Barsortfactory = function(){ return (function(){
       //~ fldbar=fllbar
     }
       
-    //cntofsub[h] is the freq of sub 
-    //destroom[fillit] is capacity of bars
+    //_cntofsub[h] is the freq of sub 
+    //_destrema[fillit] is capacity of bars
        
-    //destroom is parallel to sectppl
-    //destroom must empty but sectppl wants returned
-    //ceil of destroom is complicated here by dvrems fraction..
-    for(var i=0;i<barnm;i++){ sectppl[i]=destroom[i] }
+    //_destrema is parallel to sectppl
+    //_destrema must empty but sectppl wants returned
+    //ceil of _destrema is complicated here by dvrems fraction..
+    if(sectppl)
+    { for(var i=0;i<barnm;i++){ sectppl[i]=_destrema[i] } }
     
     for(var i=st; i<ov; i++){ 
-      var hpotofi=scores[i]
+      var subposi=scores[i]
       
-      while(destroom[destofsub[hpotofi]]===0){ destofsub[hpotofi]++ }
-      destroom[destofsub[hpotofi]]--
-      barndx[i]=destofsub[hpotofi]
+      while(_destrema[_destosub[subposi]]===0){ _destosub[subposi]++ }
+      _destrema[_destosub[subposi]]--
+      barndx[i]=_destosub[subposi]
     }
     
     
@@ -159,20 +157,23 @@ var Barsortfactory = function(){ return (function(){
     var barlen=16, reso=16
     var bars=Math.ceil(Ai.length/barlen)+1
     
-    var barix=[],barfrq=[]
+    var barix=[],barpopl=[]
     
     ranges({
       barnum: bars
      ,scores: Ai
      //~ ,st:,ov:
      ,keysbar:barix
-     ,barfreq:barfrq
+     ,barpopl:barpopl 
      ,savscore:1
      ,resolution:reso
     })
+            
+    //~ console.log("barx",barix.length,barpopl)
+
+    barix=barstoix(barix,barpopl)
+
         
-    barix=barstoix(barix,barfrq)
-    
     if(!insertndx(Ai,barix)){
       barix=stndindex(Ai,barix)
     }
@@ -195,13 +196,14 @@ var Barsortfactory = function(){ return (function(){
     for(var i=0,e=barix.length;i<e;i++){
       ind[bfill[barix[i]]++]=i
     }
-    
+
     return ind
   }
     
+    
   function insertndx(Av,Ax){ 
     
-    var lim=0
+    var lim=0, limlim=200000*Ax.length //bails at *100k
     //~ if(!Ax){ 
       //~ Ax=new Array(Av.length)
       //~ for(var e=Ax.length,c=0;c<e;c++){ Ax[c]=c}
@@ -213,20 +215,56 @@ var Barsortfactory = function(){ return (function(){
       while( bacway>=0 && Av[Ax[bacway]] > pv){ //if pre is smaller
         Ax[bacway+1] = Ax[bacway]              //
         bacway = bacway - 1
-        lim++
       }
+      lim+=pick-bacway-1
       Ax[bacway+1] = pick                     //put pick down
-      if(lim>400000000){ return false }
+      if(lim>limlim){ 
+        console.log("bail"); 
+        return false }
     }
 
     return true
   }
   
+ 
+  function insertndxx(Av,Ax){ 
+    
+    var lim=0, limlim=200000*Ax.length //bails at *100k
+    var ej=Ax.length, ek=Math.floor(ej*0.375)
+    
+    for(var bacway=0,dueway=1 ;dueway<ej; bacway=dueway++){
+      var pick = Ax[dueway], pv=Av[pick] 
+      if( dueway<ek && pv > Av[Ax[ej-dueway]] )
+      { pick=Ax[ej-dueway]
+        pv=Av[pick] //?
+        Ax[ej-dueway]=Ax[dueway] 
+        if( pv > Av[Ax[ek+dueway]] ) 
+        { Ax[dueway]=pick  //dueway is  free/overwritten by pick
+          pick=Ax[ek+dueway]
+          Ax[ek+dueway]=Ax[dueway]
+        }
+      }
+      while( bacway>=0 && Av[Ax[bacway]] > pv){ //if pick is smaller
+        Ax[bacway+1] = Ax[bacway--]              //move move them up
+      }
+      lim+=pick-bacway-1
+      if(lim>limlim){ 
+        console.log("bail"); 
+        return false 
+      }
+
+      Ax[bacway+1] = pick        //put pick down its larger
+    }
+  }
+   
+    
   return{
      bars      : ranges
+    
     ,fullindex : fullindex
     ,insertndx : insertndx
     ,stndindex : stndindex
+    
     ,barstoix  : barstoix
     ,version   : version 
   }
@@ -245,3 +283,4 @@ if (typeof exports !== 'undefined')
   else
   { (1,eval)('this')[mdname] = facfnc({}) } 
 }
+`
